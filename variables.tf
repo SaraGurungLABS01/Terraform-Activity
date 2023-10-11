f35862f47a142a85795aa18bc8c81d54fb677808
Saraswati Gurung <140760966+SaraGurungLABS01@users.noreply.github.com>
2023-10-11 11:16:23 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "instance_type" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "subnet_id" {}
-variable "security_groups" {}
+variable "region" {}
 variable "key_name" {}
 variable "ami" {}
 variable "instance_name" {}
